d12c0b3f0ab798e03dddb0ae3e4d7b448d23aae2
kornivar <kornivar62@gmail.com>
2025-10-25 15:55:24 +0300

    feat : Added game board display and move selection

--- Program.cs
+++ Program.cs
@@ -2,3 +2,30 @@
 int moves = 0;
 char player = 'X';
 bool gameOver = false;
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        board[i, j] = ' ';
+    }
+}
+
+while (!gameOver)
+{
+    Console.WriteLine("\nCurrent board:");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            Console.Write(board[i, j]);
+            if (j < 2) Console.Write("|");
+        }
+        Console.WriteLine();
+        if (i < 2) Console.WriteLine("-+-+-");
+    }
+
+    Console.WriteLine("\nPlayer " + player + ", enter your move (row and column 1-3):");
+    int row = int.Parse(Console.ReadLine()) - 1;
+    int col = int.Parse(Console.ReadLine()) - 1;
+}
