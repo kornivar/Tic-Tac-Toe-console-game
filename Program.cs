c668f1b4527b3057569c7d9a4b8f35210a06497b
kornivar <kornivar62@gmail.com>
2025-10-26 23:26:10 +0200

    feat : Added game logic and winner determination

--- Program.cs
+++ Program.cs
@@ -28,4 +28,49 @@ while (!gameOver)
     Console.WriteLine("\nPlayer " + player + ", enter your move (row and column 1-3):");
     int row = int.Parse(Console.ReadLine()) - 1;
     int col = int.Parse(Console.ReadLine()) - 1;
+
+    if (row < 0 || row > 2 || col < 0 || col > 2 || board[row, col] != ' ')
+    {
+        Console.WriteLine("Invalid move! Try again.");
+        continue;
+    }
+
+    board[row, col] = player;
+    moves++;
+
+    bool win = false;
+
+    for (int i = 0; i < 3; i++)
+    {
+        if ((board[i, 0] == player && board[i, 1] == player && board[i, 2] == player) ||
+            (board[0, i] == player && board[1, i] == player && board[2, i] == player))
+        {
+            win = true;
+            break;
+        }
+    }
+
+    if (!win)
+    {
+        if ((board[0, 0] == player && board[1, 1] == player && board[2, 2] == player) ||
+            (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player))
+        {
+            win = true;
+        }
+    }
+
+    if (win)
+    {
+        Console.WriteLine("\nPlayer " + player + " wins!");
+        gameOver = true;
+    }
+    else if (moves >= 9)
+    {
+        Console.WriteLine("\nIt's a draw!");
+        gameOver = true;
+    }
+    else
+    {
+        player = (player == 'X') ? 'O' : 'X';
+    }
 }
